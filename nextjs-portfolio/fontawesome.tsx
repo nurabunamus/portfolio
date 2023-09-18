c15bbb8e2db996bad1ddc248a29c7a31ae8eb9af
Nur <noorin.sa.99@gmail.com>
2023-09-18 13:27:47 +0300

    Portfolio completed

--- nextjs-portfolio/fontawesome.tsx
+++ nextjs-portfolio/fontawesome.tsx
@@ -1,26 +1,24 @@
-// fontawesome.tsx
-
 import { library } from '@fortawesome/fontawesome-svg-core';
-import {
+import { 
   faHtml5,
   faCss3,
   faJs,
   faReact,
-  faNodeJs,
+  faNode,
   faGithub,
   faGit,
   faSwagger,
   faDatabase,
   faJest,
   faFileAlt,
-} from '@fortawesome/free-solid-svg-icons';
+} from '@fortawesome/free-brands-svg-icons';
 
 library.add(
   faHtml5,
   faCss3,
   faJs,
   faReact,
-  faNodeJs,
+  faNode,
   faGithub,
   faGit,
   faSwagger,
